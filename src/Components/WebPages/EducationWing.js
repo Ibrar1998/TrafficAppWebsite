81ff608338a37a8768e11e706c9de4159914f884
ibrar_gill <58860119+Ibrar1998@users.noreply.github.com>
2021-08-18 21:58:29 +0500

    Delete src directory

--- src/Components/WebPages/EducationWing.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function EducationWing() {
-  return (
-    <>  
-      <h1 className='home'>EducationWing</h1>
-    </>
-  );
-}
--- src/config.js
+++
@@ -1,5 +0,0 @@
-
-const API_URL='http://192.168.0.109:7777';
-export default API_URL;
-
-// https://trafficappserver.herokuapp.com/ http://192.168.0.109:7777
